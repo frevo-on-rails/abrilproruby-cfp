104f4033a723a54734e99b69d82eab6341f2fc2c
Sérgio Fontes <sergiofontes@gmail.com>
2014-01-31 17:51:00 -0300

    Adds a new layout for the end of C.F.P.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AbrilProRubyCfp::Application.routes.draw do
-  resources :proposals, except: [:edit, :update, :destroy]
+  resources :proposals, only: :show
 
-  root to: redirect('/proposals/new')
+  root 'public#index'
 end
